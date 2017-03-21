faf4e81e6607da95be6a9cf95f9cb618bd11fc78
nhering <nhering1@gmail.com>
2017-03-21 09:29:33 -0700

    Validate source and destination paths. Pull list of files from source folder.

--- FileMover.py
+++ FileMover.py
@@ -1,11 +1,15 @@
 import shutil
 import os
 from os import listdir
+from time import ctime
 
 sourcePath = ""
 destinationPath = ""
 
+#Currently set to 24 hours ago. This can be used later to accept a user entered time for validating if a file should be moved or not.
+moveTime = time() - 86400
 
+#checks the source and destination paths given to ensure they are valid and not equal to each other
 def validatePath(path):
     if (sourcePath == destinationPath):
         print("Source and destination are the same. Exiting")
@@ -17,11 +21,12 @@ def validatePath(path):
             return ("invalid path. Exiting")
             exit()
 
-
+#prompt for source path
 sourcePath = raw_input("Enter the path of the source folder: ")
 validatePath(sourcePath)
 print(validatePath(sourcePath) + sourcePath)
 
+#prompt for destination path
 destinationPath = raw_input("Enter the path of the destination folder: ")
 validatePath(destinationPath)
 print(validatePath(destinationPath) + destinationPath)
@@ -29,5 +34,10 @@ print(validatePath(destinationPath) + destinationPath)
 filesInFolder = listdir(sourcePath)
 
 for i in filesInFolder:
-    print(sourcePath + "\\" + i)
-    shutil.move((sourcePath + "\\" + i), (destinationPath + "\\" + i))
+    fileCreateTime = os.path.getctime(sourcePath + "\\" + i)
+    if (fileCreateTime < moveTime):
+        shutil.move((sourcePath + "\\" + i), (destinationPath + "\\" + i))
+        print(i + "\nhas been moved.\n")
+    else:
+        print(i + "\nhas not been moved.\n")
+
